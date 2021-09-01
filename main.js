fd30b7674866118dde3b6327b0b405b2080e95e5
Isher Sagay <ishersagay@gmail.com>
2021-09-01 23:04:55 +0100

    PvP works and PvA works as well, however I need to add css to the code, Furthermore I need to get player cards to display on the users

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let suits = ["Spades", "Diamonds", "Clubs", "Hearts"];
 let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let number_Player = document.getElementById(`number_Player`);
 let number_Computer = document.getElementById(`number_Computer`);
+let number_PlayerTwo = document.getElementById(`numberTwo`);
 let game_Over = document.getElementById(`gameOverScreen`);
 var start__Game = document.getElementById(`clickGame`);
 let deck = [];
@@ -9,6 +10,7 @@ let player__One = [];
 let bust__One = false;
 let tally__One = 0;
 let player__Two = [];
+let tally__Two = 0;
 let bust__Two = [];
 let ai = [];
 let ai__tally = 0;
@@ -30,12 +32,39 @@ const createDeck = () => {
     }
     return deck;
 }
+const playAgain = () => {
+    player__One = [];
+    deck = [];
+    bust__One = false;
+    tally__One = 0;
+    ai = [];
+    ai__tally = 0;
+    tally__Two = 0;
+    player__Two = [];
+    gameEnd = false;
+    number_Computer.innerHTML = 0;
+    number_Player.innerHTML = 0;
+    number_PlayerTwo.innerHTML = 0;
+
+    document.getElementById(`choice`).style.display = "none";
+    document.getElementById(`startGame`).style.display = "none";
+    document.getElementById(`computer_Screen`).style.display = "none";
+    document.getElementById(`readyP1`).style.display = "none";
+    document.getElementById(`playerScreenTwo`).style.display = "none";
+    document.getElementById(`gameOverScreen`).style.display = "none";
+    document.getElementById(`clickGame`).style.display = "inline-block";
+}
 const hasAi = () => {
     document.getElementById(`choice`).style.display = "none";
     document.getElementById('startGame').style.display = "block";
+    document.getElementById(`computer_Screen`).style.display = "block";
+    document.getElementById(`readyP1`).style.display = "block";
 }
 const hasPvp = () => {
-
+    document.getElementById(`choice`).style.display = "none";
+    document.getElementById(`readyP1`).style.display = "block";
+    document.getElementById(`startGame`).style.display = "block";
+    document.getElementById(`playerScreenTwo`).style.display = "block";
 }
 const twist = (card) =>  {
     let tempVal = deck[getRandom()];
@@ -56,30 +85,37 @@ const twist = (card) =>  {
          }
     }
 }
+const stick = () => {gameEnd = true;}
 const computer = () => {
     ai__tally += twist(ai).number;
     if (ai__tally > 21) {
         game_Over.style.display = "block";
-        document.getElementById(`player_Who`).innerHTML = "AI has Won"
+        document.getElementById(`player_Who`).innerHTML = "AI has went bust"
     }
     number_Computer.innerHTML = ai__tally;
     console.log(ai);
 }
-const stick = () => {
-    gameEnd = true;
-}
 const playerOne = () => {
     tally__One += twist(player__One).number;
     if (tally__One > 21) {
-        bust__One = true;
         game_Over.style.display = "block";
-        document.getElementById(`player_Who`).innerHTML = "Player One has Won"
+        document.getElementById(`player_Who`).innerHTML = "Player One has went bust";
     }
     number_Player.innerHTML = tally__One;
     console.log(player__One)
+}
+const playerTwo = () => {
+    tally__Two += twist(player__Two).number;
+    if (tally__Two > 21) {
+        game_Over.style.display = "block";
+        document.getElementById(`player_Who`).innerHTML = "Player Two has went bust"
+    }
+    number_PlayerTwo.innerHTML = tally__Two;
+    console.log(player__Two)
 }
 const startGame = () => {
     start__Game.style.display = "none";
     document.getElementById(`choice`).style.display = "block";
     createDeck();
 }
+
